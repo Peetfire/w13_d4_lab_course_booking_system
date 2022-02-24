4af08f0e72e8b3a325b13223a1fc8c40c19d5cb2
petermordaunt <peetfire@hotmail.com>
2022-02-24 14:24:21 +0000

    Function - added show route for BookingRepository & CourseRepository and index for course Repository. Testing - routes tested in browser - all passing.

--- Course_booking_sys/src/main/java/com/PeterM/example/Course_booking_sys/controllers/CourseController.java
+++ Course_booking_sys/src/main/java/com/PeterM/example/Course_booking_sys/controllers/CourseController.java
@@ -1,4 +1,28 @@
 package com.PeterM.example.Course_booking_sys.controllers;
 
+import com.PeterM.example.Course_booking_sys.modules.Course;
+import com.PeterM.example.Course_booking_sys.repositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CourseController {
+    @Autowired
+    CourseRepository courseRepository;
+
+    @GetMapping(value = "/courses")
+    public ResponseEntity<List<Course>> getAllCourses(){
+        return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/courses/{id}")
+    public ResponseEntity getCourse(@PathVariable Long id){
+        return new ResponseEntity(courseRepository.findById(id), HttpStatus.OK);
+    }
 }
