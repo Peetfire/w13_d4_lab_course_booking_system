6eabca62fdc8b36a8337e8431fcb1819156a4741
petermordaunt <peetfire@hotmail.com>
2022-02-24 11:08:04 +0000

    Setup - empty project initialised vis spring. planning - class and table diagrams

---
+++ Course_booking_sys/src/main/java/com/PeterM/example/Course_booking_sys/CourseBookingSysApplication.java
@@ -0,0 +1,13 @@
+package com.PeterM.example.Course_booking_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseBookingSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseBookingSysApplication.class, args);
+	}
+
+}
---
+++ Course_booking_sys/src/test/java/com/PeterM/example/Course_booking_sys/CourseBookingSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PeterM.example.Course_booking_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBookingSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
